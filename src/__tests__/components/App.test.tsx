6551870e3bb94e4b6c0ee1025412e45257604f98
ScottyCompton <scott.lonis@outlook.com>
2022-08-20 19:47:45 -0500

    uninstalled firebase & mdb toolkit, installed material ui

--- src/__tests__/components/App.test.tsx
+++ src/__tests__/components/App.test.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from 'components/App'
 
-test('renders learn react link', () => {
+test('renders the app component', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
